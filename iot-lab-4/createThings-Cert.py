55b023f98186a94f5f761ea51afb7df67b7bcb48
Deylis <deyliscano89@gmail.com>
2024-03-26 04:45:51 -0400

    Commenting the code.

--- iot-lab-4/createThings-Cert.py
+++ iot-lab-4/createThings-Cert.py
@@ -1,17 +1,21 @@
-################################################### Connecting to AWS
+# Connecting to AWS: Imports boto3 library for interacting with AWS services and json for handling JSON data
 import boto3
 import json
-################################################### Create random name for things
+
+# Importing this module to be used to create random name for things
 import random
 import string
 
-################################################### Parameters for Thing
+# Parameters for Thing
 defaultPolicyName = 'policy_car' # My policy name
 thingGroupName = 'car_group'  # Thing group name to attach the 500 things
+
 ###################################################
 
-def createThing(): # Method to create a thing
-    thingName = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(15)])
+# createThing() Function: Creates a thing in AWS IoT Core. 
+# It generates a random name for the thing, creates a certificate for the thing, and adds the thing to a specified thing group.
+def createThing(): 
+    thingName = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(15)]) # Generates random names for the IoT things to be created using a combination of lowercase and uppercase letters along with digits
     global thingClient
     thingResponse = thingClient.create_thing(
         thingName=thingName
@@ -23,9 +27,13 @@ def createThing(): # Method to create a thing
         elif element == 'thingId':
             thingId = data['thingId']
             createCertificate(thingName) # Create a certificate for this thing
-            addThingToThingGroup(thingName)  # Add the thing to the thing group 
+            addThingToThingGroup(thingName)  # Add the thing to a thing group 
 
-def createCertificate(thingName):  # Method to create a certificate for a thing
+######################################################
+
+# createCertificate() Function: Creates a certificate for a thing. 
+# It also generates public and private keys for the certificate, writes them to files, and attaches the certificate to a policy and the thing.
+def createCertificate(thingName): 
     global thingClient
     certResponse = thingClient.create_keys_and_certificate(
         setAsActive=True
@@ -58,12 +66,22 @@ def createCertificate(thingName):  # Method to create a certificate for a thing
         principal=certificateArn
     )
 
-def addThingToThingGroup(thingName): # Method to add a thing to the thing group
+#########################################################
+    
+# addThingToThingGroup() Function: Add a thing to a thing group
+def addThingToThingGroup(thingName): 
     response = thingClient.add_thing_to_thing_group(
         thingGroupName=thingGroupName,
         thingName=thingName
     )
 
+##########################################################
+    
+#Initializing an AWS IoT client using the Boto3 library    
 thingClient = boto3.client('iot')
-for i in range(500): # Loop to create 500 things
+
+##########################################################
+
+# Main Loop to create 500 things
+for i in range(500): 
     createThing()
