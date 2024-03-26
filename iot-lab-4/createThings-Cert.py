3b266420e2be06f311df1727fb3bc734585b49bd
Deylis <deyliscano89@gmail.com>
2024-03-26 04:24:03 -0400

    Adding 500 things to the thing group: car-group

--- iot-lab-4/createThings-Cert.py
+++ iot-lab-4/createThings-Cert.py
@@ -6,10 +6,11 @@ import random
 import string
 
 ################################################### Parameters for Thing
-defaultPolicyName = 'policy_car'
+defaultPolicyName = 'policy_car' # My policy name
+thingGroupName = 'car_group'  # Thing group name to attach the 500 things
 ###################################################
 
-def createThing():
+def createThing(): # Method to create a thing
     thingName = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(15)])
     global thingClient
     thingResponse = thingClient.create_thing(
@@ -21,9 +22,10 @@ def createThing():
             thingArn = data['thingArn']
         elif element == 'thingId':
             thingId = data['thingId']
-            createCertificate(thingName)
+            createCertificate(thingName) # Create a certificate for this thing
+            addThingToThingGroup(thingName)  # Add the thing to the thing group 
 
-def createCertificate(thingName):
+def createCertificate(thingName):  # Method to create a certificate for a thing
     global thingClient
     certResponse = thingClient.create_keys_and_certificate(
         setAsActive=True
@@ -40,11 +42,11 @@ def createCertificate(thingName):
         elif element == 'certificateId':
             certificateId = data['certificateId']
 
-    with open(f'public_{thingName}.key', 'w') as outfile:
+    with open(f'public_{thingName}.key', 'w') as outfile: # Public key for the thing
         outfile.write(PublicKey)
-    with open(f'private_{thingName}.key', 'w') as outfile:
+    with open(f'private_{thingName}.key', 'w') as outfile: # Private key for the thing
         outfile.write(PrivateKey)
-    with open(f'cert_{thingName}.pem', 'w') as outfile:
+    with open(f'cert_{thingName}.pem', 'w') as outfile: # Certificate for the thing
         outfile.write(certificatePem)
 
     response = thingClient.attach_policy(
@@ -56,6 +58,12 @@ def createCertificate(thingName):
         principal=certificateArn
     )
 
+def addThingToThingGroup(thingName): # Method to add a thing to the thing group
+    response = thingClient.add_thing_to_thing_group(
+        thingGroupName=thingGroupName,
+        thingName=thingName
+    )
+
 thingClient = boto3.client('iot')
-for i in range(500):
+for i in range(500): # Loop to create 500 things
     createThing()
